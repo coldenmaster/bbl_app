a9527bcd2367c8530dc6dad0eb2602b878f4b147
coldenmaster <15894368@qq.com>
2024-02-16 16:58:04 +0800

    v0.0.6

--- bbl_app/func.py
+++ bbl_app/func.py
@@ -1,7 +1,7 @@
 import frappe
-from bbl_api.utils import print_green, print_red
-from wechat_work.utils import send_str_to_admin
-
+# from wechat_work.utils import send_str_to_admin
+# import wechat_work
+import wechat_work.utils
 from frappe import _
 from frappe.utils import get_fullname
 
@@ -11,19 +11,24 @@ def get_home_page(user):
     if (user == "Guest"):
         return "guest_me"
     
-    
+#  importlib.reload(wechat_work.utils)
 def on_login(login_manager):
     if login_manager.user != "Guest":
         subject = _("{0} logged in").format(get_fullname(login_manager.user))
         # send_str_to_admin(subject)
-        frappe.enqueue("send_str_to_admin", arg1 = subject)
+        frappe.enqueue(wechat_work.utils.send_str_to_admin, msg = subject)
 
 
 def after_insert_all(doc, method=None):
-	if isinstance(doc, str):
-		doctype = doc  # assuming doctype name was passed directly
-	else:
-		doctype = doc.doctype
-
-	if doctype == "Mold":
-		print_red(f"after_insert_all {doctype}")
+    if isinstance(doc, str):
+        doctype = doc  # assuming doctype name was passed directly
+    else:
+        doctype = doc.doctype
+    user = get_fullname()
+    if doctype == "Mold":
+        frappe.enqueue(wechat_work.utils.send_str_to_admin, msg = f'{user}:新建模具成功')
+        print(f"after_insert_all {doctype}")
+    if doctype == "Product Weight Record":
+        frappe.enqueue(wechat_work.utils.send_str_to_admin, msg = f'{user}:新建下料重量成功')
+    if doctype == "Forge Process Record":
+        frappe.enqueue(wechat_work.utils.send_str_to_admin, msg = f'{user}:新建锻造生产记录成功')
