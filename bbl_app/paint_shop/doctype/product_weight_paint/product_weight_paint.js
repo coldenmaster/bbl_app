66636fd46f65ec4c33b0ce2348cb65ac3e6b2040
coldenmaster <15894368@qq.com>
2024-02-28 16:34:42 +0800

    v0.0.7.2

--- bbl_app/paint_shop/doctype/product_weight_paint/product_weight_paint.js
+++ bbl_app/paint_shop/doctype/product_weight_paint/product_weight_paint.js
@@ -9,46 +9,48 @@ frappe.ui.form.on("Product Weight Paint", {
     //         frm.set_value("product_type", product_doc.product_type);
     //         frm.save()
     //     });
-	},
+	// },
     product_name(frm) {
-        frappe.db.get_doc("Product Name", frm.doc.product_name).then((product_doc) => {
-            console.log("product_doc", product_doc);
-            frm.set_value("semi_product", product_doc.semi_product_name);
-            frm.set_value("product_type", product_doc.product_type);
-            // frappe.db.get_doc("Semi Product", product_doc.semi_product).then((semi_product_doc) => {
-            //     console.log("semi_product_doc", semi_product_doc);
-            // });
-        });
+        frappe.db.get_doc("Product Name", frm.doc.product_name)
+            .then((product_doc) => {
+                console.log("product_doc", product_doc);
+                frm.set_value("semi_product", product_doc.semi_product_name);
+                frm.set_value("product_type", product_doc.product_type);
+                // frappe.db.get_doc("Semi Product", product_doc.semi_product).then((semi_product_doc) => {
+                //     console.log("semi_product_doc", semi_product_doc);
+                // });
+            });
     },
     weight_one(frm) {
-        set_average_weight(frm);
+        // set_average_weight(frm);
+        frm.trigger("set_average_weight")
     },
     weight_two(frm) {
-        set_average_weight(frm);
+        frm.trigger("set_average_weight")
     },
     weight_three(frm) {
-        set_average_weight(frm);
+        frm.trigger("set_average_weight")
+    },
+    set_average_weight(frm) {
+        console.log("average_weight");
+        weight_count = 0;
+        total_weight = 0;
+        weight = frm.doc.weight_one;
+        if (weight) {
+            total_weight += weight;
+            weight_count += 1;
+        }
+        weight = frm.doc.weight_two;
+        if (weight) {
+            total_weight += weight; 
+            weight_count += 1;
+        }
+        weight = frm.doc.weight_three;
+        if (weight) {
+            total_weight += weight;
+            weight_count += 1;
+        }
+        frm.set_value("product_weight", total_weight / weight_count);
     }
-});
 
-set_average_weight = function(frm) {
-    console.log("average_weight");
-    weight_count = 0;
-    total_weight = 0;
-    weight = frm.doc.weight_one;
-    if (weight) {
-        total_weight += weight;
-        weight_count += 1;
-    }
-    weight = frm.doc.weight_two;
-    if (weight) {
-        total_weight += weight; 
-        weight_count += 1;
-    }
-    weight = frm.doc.weight_three;
-    if (weight) {
-        total_weight += weight;
-        weight_count += 1;
-    }
-    frm.set_value("product_weight", total_weight / weight_count);
-}
+});
